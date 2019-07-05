839aeb7006bebf2a54a0f9e04214ece9d8feb806
Jannis R <mail@jannisr.de>
2019-07-05 13:26:08 +0200

    init

---
+++ publish.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict'
+
+const mri = require('mri')
+const pkg = require('./package.json')
+
+const argv = mri(process.argv.slice(2), {
+	boolean: [
+		'help', 'h',
+		'version', 'v'
+	]
+})
+
+if (argv.help || argv.h) {
+	process.stdout.write(`
+Usage:
+    echo 'a new message' | publish-to-pubsub-topic <topic>
+\n`)
+	process.exit(0)
+}
+
+if (argv.version || argv.v) {
+	process.stdout.write(`${pkg.name} v${pkg.version}\n`)
+	process.exit(0)
+}
+
+// todo
